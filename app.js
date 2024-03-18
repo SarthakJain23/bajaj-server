82b8c09faa0d2ee59c0022c36d0913d898f7f6d6
Sarthak Jain <sarthak1280.be21@chitkara.edu.in>
2024-03-18 11:41:43 +0530

    Commit

--- app.js
+++ app.js
@@ -17,29 +17,31 @@ app.get("/", (req, res) => res.send("<h1>Site is working here</h1>"));
 
 app.post("/bfhl", (req, res) => {
   const data = req.body.data;
-  console.log(data);
-  const evenNum = [];
-  const oddNum = [];
-  const alp = [];
-  data.forEach((item) => {
-    if (typeof data[i] == "number") {
-      if (data[i] % 2 == 0) {
-        evenNum.push(data[i]);
-      } else {
-        oddNum.push(data[i]);
-      }
-    } else {
-      alp.push(data[i]);
-    }
-  });
   return res.status(200).json({
-    is_success: true,
-    user_id: "sarthak_jain_23052003",
-    email: "sarthak1280.be21@chitkara.edu.in",
-    roll_number: "2110991280",
-    odd_numbers: oddNum,
-    even_numbers: evenNum,
-    alphabets: alp,
+    data : data
+  })
+  // const evenNum = [];
+  // const oddNum = [];
+  // const alp = [];
+  // data.forEach((item) => {
+  //   if (typeof data[i] == "number") {
+  //     if (data[i] % 2 == 0) {
+  //       evenNum.push(data[i]);
+  //     } else {
+  //       oddNum.push(data[i]);
+  //     }
+  //   } else {
+  //     alp.push(data[i]);
+  //   }
+  // });
+  // return res.status(200).json({
+  //   is_success: true,
+  //   user_id: "sarthak_jain_23052003",
+  //   email: "sarthak1280.be21@chitkara.edu.in",
+  //   roll_number: "2110991280",
+  //   odd_numbers: oddNum,
+  //   even_numbers: evenNum,
+  //   alphabets: alp,
   });
 });
 
